993b322f1dbd5f93dfe83b994392f0184cc442c1
hbb_mac <kok.bing@qq.com>
2025-09-20 16:44:57 +0800

    feat - andorid first commit

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,14 @@
 /**
  * @type {import('@react-native-community/cli-types').UserDependencyConfig}
+        cxxModuleHeaderName: 'MyHeader', // CLI will include this header while linking.
  */
 module.exports = {
   dependency: {
     platforms: {
       android: {
-        cmakeListsPath: 'generated/jni/CMakeLists.txt',
+        sourceDir: 'android',
+        cxxModuleCMakeListsPath: `CMakeLists.txt`, // This is relative to the sourceDir.
+        cxxModuleCMakeListsModuleName: 'prism-rn', // This is the name of the CMake target.
       },
     },
   },
